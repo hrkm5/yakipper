1554f5c6d4665e75799012a80ffc4dfa95525616
Hiroki Morioka <hmorioka@mba-007952.ecr.box.net>
2020-05-23 15:32:21 +0900

    ver1.0.0

---
+++ popup.js
@@ -0,0 +1,48 @@
+const BOX_SUPPORT_HOST = "support.box.com" // Box Support Site
+
+const extract_box_support_url = (rawUrl) => {
+    const url = new URL(rawUrl);
+    if (url.host == BOX_SUPPORT_HOST && url.pathname.match(/\/hc\/ja\/articles\/[A-Za-z0-9]+-\S+/)) {
+      newUrl = url.origin + url.pathname.replace(/(\/hc\/ja\/articles\/[A-Za-z0-9]+)(\-\S+)/, '$1');
+      console.log(newUrl);
+      return newUrl;
+    } else {
+      return rawUrl;
+    }
+  }
+
+const Get_URLnTitle = () => {
+    // Get the current page's URL and Title
+    // https://developer.chrome.com/extensions/tabs
+    chrome.tabs.query({ active: true, currentWindow: true, lastFocusedWindow: true },(tabs) => {
+      let txt = '';
+      let url = tabs[0].url;
+      const title = tabs[0].title;
+
+      // Checking Box Support Site
+      // url = extract_box_support_url(url);
+
+      // Decoding URL
+
+      url = decodeURI(url);
+
+      // store Title and URL to textarea and copy
+      document.querySelector('#txt').value = '* ' + title + '\n' + url;
+      copy();
+    })
+  }
+
+// Copy to clipboad
+// https://qiita.com/yukuduri/items/3f2159ab3cec95a1ba28
+
+const copy = () => {
+    const copyText = document.querySelector('#txt');
+    copyText.select();
+    document.execCommand('copy');
+    console.log("Copied");
+}
+
+window.addEventListener('load',()=>{
+    Get_URLnTitle();
+    document.querySelector("#copy").addEventListener("click", copy);
+  })
